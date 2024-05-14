4906bfcc8695f8787d22bb52fa86ae56cbc715aa
Mihail Pugachev <xd393727@gmail.com>
2024-05-14 23:57:13 +0300

    add model finance

--- migrations/env.py
+++ migrations/env.py
@@ -18,9 +18,9 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from models import goods
+from models import goods, finance
 
-target_metadata = goods.Base.metadata
+target_metadata = [goods.Base.metadata, finance.Base.metadata]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- models/finance.py
+++ models/finance.py
@@ -6,7 +6,19 @@ from sqlalchemy import Integer, ForeignKey, DATETIME, String, Column, Boolean
 Base = declarative_base()
 
 
+class Operation(Base):
+    __tablename__ = "operation"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    operation_name = Column(String, unique=True)
+    name = relationship("Finance", back_populates="name_operation")
+
+
 class Finance(Base):
     __tablename__ = "finance"
 
-    name_operation = Column()
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    operation_id = Column(ForeignKey('operation.id'))
+    name_operation = relationship("Operation", back_populates="name")
+    create_datetime = Column(DATETIME, default=datetime.datetime.now())
+    balance = Column(Integer)
